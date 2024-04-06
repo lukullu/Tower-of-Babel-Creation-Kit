c807b8761dfc233eb887c8efe6dc64f6e3bb486d
Lukullu <lukaswestram@icloud.com>
2024-04-06 02:13:44 +0200

    fix: make projectiles ttl adjustable

--- game/src/main/java/com/lukullu/undersquare/objectTypes/Projectile.java
+++ game/src/main/java/com/lukullu/undersquare/objectTypes/Projectile.java
@@ -7,8 +7,8 @@ public class Projectile extends Debris
 {
     public ISegmentedObject origin;
 
-    public Projectile(Entity origin, String psff_resource, Vec2 position, double rotation, double scaling, Vec2 initForce) {
-        super(psff_resource, position, rotation, scaling,10);
+    public Projectile(Entity origin, String psff_resource, Vec2 position, double rotation, double scaling, Vec2 initForce, double timeToLive) {
+        super(psff_resource, position, rotation, scaling, timeToLive);
         this.origin = origin;
         force = initForce;
         coefficientOfFriction = 0;
